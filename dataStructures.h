b090c0aee3a99ba0bd8db59e3975ad829e0c5491
Muhammad Faisal <mfaisal@aucegypt.edu>
2019-05-05 17:53:18 +0200

    putting classes skeleton

---
+++ dataStructures.h
@@ -0,0 +1,77 @@
+#include <vector>
+
+using namespace std;
+
+//! To DO:
+//1) assume only y is the output
+
+
+
+class spiceOperand{
+    private:
+
+
+
+    public:
+    spiceOperand();
+    ~spiceOperand();
+}
+
+
+
+
+
+class simpleOperand(){
+
+
+}
+
+
+class expressionOperand(){
+
+
+    
+}
+
+
+
+
+
+
+class spiceOperator{
+private:
+    /* data */
+public:
+    spiceOperator();
+
+    ~spiceOperator();
+};
+
+
+spiceOperator::spiceOperator(/* args */)
+{
+}
+
+spiceOperator::~spiceOperator()
+{
+}
+
+
+
+
+class spiceAnd{
+
+private:
+    
+    
+public:
+    spiceAnd()
+    ~spiceAnd();
+
+};
+
+spiceAnd::spiceAnd{
+}
+
+spiceAnd::~spiceAnd{
+}
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vecctor>
+
+using namespace std;
+
+
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+    return 0;
+}
